d1c347cc16f89e39fb30b455db2a28337c07e471
Rajani <rajani.kami004@gmail.com>
2026-03-29 19:34:01 -0400

    lab 10 documentation

---
+++ Lab10_rbaraili-1.py
@@ -0,0 +1,9 @@
+
+""" The program's name:Word Count 
+Your name: Rajani Baraili
+The purpose of the program: displays menu of 4 file, lets user choose one and reads and analyze that file and count the frequency of every
+word selected file and prints alphabetical report.
+Any info about starter code: none
+Date: 03/29/2026 """
+
+
